c4d55f127dd9438f259e4e56bf95342d46276029
SunYufei <sunyufei963@gmail.com>
2020-03-30 14:02:03 +0800

    Update: 2020/3/30

--- cyf.py
+++ cyf.py
@@ -6,7 +6,7 @@ from PIL import Image, ImageDraw, ImageFont
 def draw(pic: str, font_size: int = 10):
     img = cv2.imread(pic)
     img = img[:, :, (2, 1, 0)]
-    blank = Image.new('RGB', [len(img[0]), len(img[0])], 'white')
+    blank = Image.new('RGB', [len(img[0]), len(img)], 'white')
     draw_obj = ImageDraw.Draw(blank)
     font = ImageFont.truetype('C:/Windows/Fonts/Microsoft YaHei UI/msyhbd.ttc', size=font_size - 1)
     for i in range(0, len(img), font_size):
@@ -15,13 +15,9 @@ def draw(pic: str, font_size: int = 10):
             draw_obj.ink = img[i][j][0] + img[i][j][1] * 256 + img[i][j][2] * 256 * 256
             draw_obj.text([j, i], text[j // font_size % len(text)], font=font)
     cv2.imshow('Demo', np.asarray(blank))
-    if font_size < 5:
-        cv2.waitKey(100)
-    elif font_size < 10:
-        cv2.waitKey(300)
-    else:
-        cv2.waitKey(800)
 
 
-for s in range(3, 16):
-    draw('img/cyf.jpg', s)
+cv2.imshow('Demo', cv2.imread('img/cyf.jpg'))
+cv2.waitKey(1500)
+draw('img/cyf.jpg', 10)
+cv2.waitKey(3000)
